73192d43b534fb4583f5d2ba5a3823e00d3290e4
William Zhou <williamkzhou@hotmail.com>
2019-08-02 14:03:11 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,25 @@
+region = ""
+
+aws_profile = ""
+
+shared_credentials_file = ""
+
+key_name = ""
+
+hosted_zone_id = ""
+
+bastion_sg_id = ""
+
+jenkins_username = ""
+
+jenkins_password = ""
+
+jenkins_credentials_id = ""
+
+vpc_id = ""
+
+vpc_private_subnets = []
+
+vpc_public_subnets = []
+
+ssl_arn = ""
